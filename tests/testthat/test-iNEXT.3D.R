baace917f4dc86644ee58ad445f3b32c7e94b839
KaiHsiangHu <river19951224@gmail.com>
2023-10-13 17:21:00 +0800

    Update manual.

--- tests/testthat/test-iNEXT.3D.R
+++ tests/testthat/test-iNEXT.3D.R
@@ -1,67 +1,67 @@
-context("iNEXT.3D")
-test_that("iNEXT.3D for abundance-based data", {
+context("iNEXT3D")
+test_that("iNEXT3D for abundance-based data", {
   # Test input by a demo data
-  data(dunes)
-  out <- iNEXT3D(dunes$data, q = 0, datatype = "abundance")
-  expect_is(out, "iNEXT.3D")
+  data("Brazil")
+  out <- iNEXT3D(Brazil$data, q = 0, datatype = "abundance")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "n")
-  expect_equal(nrow(out$DataInfo), length(dunes))
+  expect_equal(names(out$TDInfo)[2], "n")
+  expect_equal(nrow(out$TDInfo), length(Brazil$data))
   
   # Test input by a vector
-  x <- dunes$data$EM
+  x <- Brazil$data$Edge
   out <- iNEXT3D(x, q = 0, datatype = "abundance")
-  expect_is(out, "iNEXT.3D")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "n")
-  expect_equal(nrow(out$DataInfo), 1)
+  expect_equal(names(out$TDInfo)[2], "n")
+  expect_equal(nrow(out$TDInfo), 1)
   
   # Test input by a data.frame
   x <- data.frame(a = c(10,20,30,40,50,0,0), b = c(11,22,0,0,33,44,55))
   out <- iNEXT3D(x, q = 0, datatype = "abundance")
-  expect_is(out, "iNEXT.3D")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "n")
-  expect_equal(nrow(out$DataInfo), ncol(x))
+  expect_equal(names(out$TDInfo)[2], "n")
+  expect_equal(nrow(out$TDInfo), ncol(x))
   
 })
 
-test_that("iNEXT.3D for sampling-unit-based incidence frequencies data", {
+test_that("iNEXT3D for sampling-unit-based incidence frequencies data", {
   # Test input by a demo data
-  data(ant)
-  out <- iNEXT3D(ant, q = 0, datatype = "incidence_freq")
-  expect_is(out, "iNEXT.3D")
+  data("fish")
+  out <- iNEXT3D(fish$data, q = 0, datatype = "incidence_raw")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "T")
-  expect_equal(nrow(out$DataInfo), length(ant))
+  expect_equal(names(out$TDInfo)[2], "T")
+  expect_equal(nrow(out$TDInfo), length(fish$data))
   
   # Test input by a vector
-  out <- iNEXT3D(ant$h50m, q = 0, datatype = "incidence_freq")
-  expect_is(out, "iNEXT.3D")
+  out <- iNEXT3D(c(ncol(fish$data$`1981-1985`), rowSums(fish$data$`1981-1985`)), q = 0, datatype = "incidence_freq")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "T")
-  expect_equal(nrow(out$DataInfo), 1)
+  expect_equal(names(out$TDInfo)[2], "T")
+  expect_equal(nrow(out$TDInfo), 1)
 })
 
 
-test_that("iNEXT.3D for species by sampling-units incidence matrix", {
+test_that("iNEXT3D for species by sampling-units incidence matrix", {
   # Test input by a demo data
   data(data.inc)
   options(warn=-1)
   out <- iNEXT3D(data.inc$data, q = 0, datatype = "incidence_raw", nT = data.inc$nT)
-  expect_is(out, "iNEXT.3D")
+  expect_is(out, "iNEXT3D")
   expect_output(str(out), "List of 3")
-  expect_equal(names(out$DataInfo)[2], "T")
-  expect_equal(nrow(out$DataInfo), length(data.inc$nT))
+  expect_equal(names(out$TDInfo)[2], "T")
+  expect_equal(nrow(out$TDInfo), length(data.inc$nT))
   
   # Test input by a data.frame
   # x <- ciliates$EtoshaPan
   # # expect_equal(class(x), "matrix")
-  # out <- iNEXT.3D(x, q=0, datatype="incidence_raw")
-  # expect_is(out, "iNEXT.3D")
+  # out <- iNEXT3D(x, q=0, datatype="incidence_raw")
+  # expect_is(out, "iNEXT3D")
   # expect_output(str(out), "List of 3")
-  # expect_equal(names(out$DataInfo)[2], "T")
-  # expect_equal(nrow(out$DataInfo), 1)
+  # expect_equal(names(out$TDInfo)[2], "T")
+  # expect_equal(nrow(out$TDInfo), 1)
 })
 
 
